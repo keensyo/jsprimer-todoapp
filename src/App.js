e19c9781084309eb14b9d2cc7bd3b3491c113e7a
keensyo <k66mdg55@gmail.com>
2021-06-22 01:26:39 +0900

    modified

--- src/App.js
+++ src/App.js
@@ -19,7 +19,32 @@ export class App {
             // それぞれのTodoItem要素をtodoListElement以下へ追加する
             const todoItems = this.todoListModel.getTodoItems();
             todoItems.forEach(item => {
-                const todoItemElement = element`<li>$(item.title)</li>`;
+                // 削除ボタン(x)をそれぞれ追加する
+                const todoItemElement = item.completed
+                    ? element`<li><input type="checkbox" class="checkbox" checked>
+                        <s>${item.title}</s>
+                        <button class="delete>x</button>
+                    </li>`
+                    : element`<li><input type="checkbox" class="checkbox">
+                        ${item.title}
+                        <button class="delete">x</button>
+                    </li>`
+                // チェックボックスがトグルしたときのイベントにリスナー関数を登録
+                const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
+                inputCheckboxElement.addEventListener("change", () => {
+                    // 指定したTodoアイテムの完了状態を反転させる
+                    this.todoListModel.updateTodo({
+                        id: item.id,
+                        completed: !item.completed
+                    });
+                });
+                // 削除ボタン(x)がクリックされたときにTodoListModelからアイテムを削除する
+                const deleteButtonElement = todoItemElement.querySelector(".deleted");
+                deleteButtonElement.addEventListener("click", () => {
+                    this.todoListModel.deleteTodo({
+                        id: item.id
+                    });
+                });
                 todoListElement.appendChild(todoItemElement);
             });
             //containerElementの中身をtodoListElementで上書きする
